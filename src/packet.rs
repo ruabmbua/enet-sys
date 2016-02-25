c81d1465ab68970a3f51d886f85a905a3bdb0178
Roland Ruckerbauer <roland.rucky@gmail.com>
2016-02-25 22:51:26 +0100

    packet functions and allow non_snake_case

--- src/packet.rs
+++ src/packet.rs
@@ -1,4 +1,5 @@
 use libc::*;
+use ::ENetBuffer;
 
 pub type ENetPacketFreeCallback = extern fn(packet: *mut ENetPacket);
 
@@ -10,4 +11,21 @@ pub struct ENetPacket {
     pub freeCallback: ENetPacketFreeCallback,
     pub referenceCount: size_t,
     pub userData: *mut c_void,
+}
+
+#[repr(C)]
+pub enum ENetPacketFlag {
+    ENET_PACKET_FLAG_RELIABLE = 1,
+    ENET_PACKET_FLAG_UNSEQUENCED = 1<<1,
+    ENET_PACKET_FLAG_NO_ALLOCATE = 1<<2,
+    ENET_PACKET_FLAG_UNRELIABLE_FRAGMENT = 1<<3,
+    ENET_PACKET_FLAG_SENT = 1<<8,
+}
+
+extern {
+    pub fn enet_crc32(buffers: *const ENetBuffer, bufferCount: size_t) -> uint32_t;
+    pub fn enet_packet_create(data: *const c_void, dataLength: size_t, flags: uint32_t)
+            -> *mut ENetPacket;
+    pub fn enet_packet_destroy(packet: *mut ENetPacket);
+    pub fn enet_packet_resize(packet: *mut ENetPacket, dataLength: size_t) -> c_int;
 }
