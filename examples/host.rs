f660d52a1805f01d9f2e7391d1de98899af51493
Shane Pearman <spearman@github.com>
2016-07-18 17:14:52 -0700

    add sizes test, fix protocol types and layouts, added ENetProtocolHeader

--- examples/host.rs
+++ examples/host.rs
@@ -2,33 +2,16 @@ extern crate enet_sys;
 
 use std::mem::size_of;
 
-use enet_sys::*;
-//use enet_sys::{enet_initialize, enet_deinitialize};
+use enet_sys::{enet_initialize, enet_deinitialize};
 use enet_sys::address::ENetAddress;
-use enet_sys::host::{enet_host_create, enet_host_destroy, ENetHost};
-use enet_sys::list::ENetList;
-use enet_sys::socket::ENetSocket;
-use enet_sys::peer::ENetPeer;
-use enet_sys::protocol::ENetProtocol;
+use enet_sys::host::{enet_host_create, enet_host_destroy};
 
 fn main() {
-    println!("size of ENetHost: {}", size_of::<ENetHost>());
-    println!("size of ENetAddress: {}", size_of::<ENetAddress>());
-    println!("size of ENetSocket: {}", size_of::<ENetSocket>());
-    println!("size of ENetInterceptCallback: {}",
-      size_of::<ENetInterceptCallback>());
-    println!("size of ENetList: {}", size_of::<ENetList>());
-    println!("size of ENetCompressor: {}", size_of::<ENetCompressor>());
-    println!("size of ENetProtocol: {}", size_of::<ENetProtocol>());
-    println!("size of ENetBuffer: {}", size_of::<ENetBuffer>());
-
-    println!("size of ENetEvent: {}", size_of::<ENetEvent>());
-    println!("size of ENetPeer: {}", size_of::<ENetPeer>());
-
-    println!("Starting test of enet bindings...");
+    println!("Starting test of host creation...");
     if unsafe {enet_initialize()} < 0 {
         panic!("Error on enet initialization.");
     }
+    println!("Enet initialized.");
 
     println!("creating ENet server host...");
 
@@ -62,7 +45,6 @@ fn main() {
       enet_host_destroy (server);
     }
 
-    println!("Enet initialized.");
     unsafe {enet_deinitialize();}
     println!("Enet deinitialized.");
 }
